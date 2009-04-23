b9d38cb98c48f1dbf318dde51324dd81f6d749de
James Nuckolls <jamesn@rubadub.inside.what.net>
2009-04-22 21:16:59 -0500

    split several of the clases out into their own files

---
+++ lib/softlayer/util.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+# util
+#
+# Author:: James Nuckolls
+# Copyright:: Copyright (c) 2009 SoftLayer. All rights reserved.
+#
+#= Description
+#
+#= ToDo
+#
+
+require 'rubygems' rescue LoadError
+gem 'soap4r'
+require 'soap/header/simplehandler'
+
+
+module SoftLayer
+  # A class to old Paramaters.
+  class Param < SOAP::Header::SimpleHandler
+    def initialize(tag, out)
+      @out = out
+      super(XSD::QName.new(nil, tag))
+    end
+
+    def on_simple_outbound
+      @out
+    end
+
+    def [](k)
+      return @out[k]
+    end
+
+    def []=(k,v)
+      @out[k]=v
+    end
+  end
+
+  # A class to hold the object mask.
+  class ObjectMask < SOAP::Header::SimpleHandler
+
+    def initialize(tag, out)
+      @out = out
+      super(XSD::QName.new(nil, tag))
+    end
+
+    def on_simple_outbound
+      { 'mask' => @out }
+    end
+
+    def [](k)
+      @out[k]
+    end
+
+    def []=(k,v)
+      @out[k]=v
+    end
+  end
+
+  class ResultLimit < SOAP::Header::SimpleHandler
+    attr_accessor :limit, :offset
+
+    # limit should be an array of two elements; limit and offset.
+    def initialize(tag, limit)
+      @limit = limit[0]
+      @offset = limit[1]
+      super(XSD::QName.new(nil, tag))
+    end
+
+    def on_simple_outbound
+      { 'limit' => @limit, 'offset' => @offset }
+    end
+  end
+
+
+  # An Exception proxy class
+  # This doesn't do anything yet, but it probably
+  # will at some point.
+  class Exception < RuntimeError
+
+    def initialize(args)
+      e = args[:exception]
+      message = args[:message] unless args[:message].nil?
+      message = e.message unless e.nil?
+      super(message)
+
+      @realException = e unless e.nil?
+      @realException = self if @realException.nil?
+    end
+  end
+end
